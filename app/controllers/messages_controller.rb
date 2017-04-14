19b2dd217de1bde8f12c83397460b0721e289d57
Mihara Sonoka <sonoka777@gmail.com>
2017-04-14 08:22:20 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
   def index
     @messages = Message.all
   end
 
   def show
-    @message = Message.find(params[:id])
   end
 
   def new
@@ -15,43 +16,41 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
 
     if @message.save
-      flash[:success] = 'Task が正常に追加されました'
+      flash[:success] = 'Message が正常に投稿されました'
       redirect_to @message
     else
-      flash.now[:danger] = 'Task が追加されませんでした'
+      flash.now[:danger] = 'Message が投稿されませんでした'
       render :new
     end
   end
 
   def edit
-    @message = Message.find(params[:id])
   end
 
   def update
-    @message = Message.find(params[:id])
-
     if @message.update(message_params)
-      flash[:success] = 'Task は正常に更新されました'
+      flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'Message は更新されませんでした'
       render :edit
     end
   end
 
   def destroy
-    @message = Message.find(params[:id])
     @message.destroy
 
-    flash[:success] = 'Task は正常に削除されました'
+    flash[:success] = 'Message は正常に削除されました'
     redirect_to messages_url
   end
-  
+
   private
 
-  # Strong Parameter
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
   def message_params
     params.require(:message).permit(:content)
   end
-  
 end
